81860be877b543165a5f4308663fa120536df40d
jmillanacosta <javier.millan.acosta@gmail.com>
2023-06-15 13:25:19 +0200

    Finished GPT-3.5 turbo analyses

--- notebooks/scripts/excel_imports.py
+++ notebooks/scripts/excel_imports.py
@@ -21,6 +21,35 @@ def append_sheets_with_organ_column(file_path):
 
     # Count rows with NaN values
     rows_with_nan = concatenated_data.isnull().any(axis=1).sum()
-    print(f"Number of rows with at least one NaN value: {rows_with_nan}/{len(concatenated_data.index)}")
-    print("Organs: " + ", ".join(list(set(concatenated_data["Organ"]))))
+    print(f"\nNumber of rows with at least one NaN value: {rows_with_nan} of {len(concatenated_data.index)}")
+    print("\nOrgans: " + ", ".join(list(set(concatenated_data["Organ"]))))
+    concatenated_data['PMID'] = concatenated_data['PMID'].astype(str)
+    concatenated_data['PEG cover'] = concatenated_data['PEG cover'].astype(str)
+    concatenated_data['Ligand'] = concatenated_data['PEG cover'].astype(str)
+    nps = "\n- ".join(list(set(concatenated_data['NP_Type'])))
+    print(f'\nIncluded NPs: \n- {nps}')
+
     return concatenated_data
+
+def describe_df(df):
+    # Print number of NaN values in each column, column element datatypes
+    nan_counts = df.isnull().sum()
+    for column, count in nan_counts.items():
+        column_data = df[column]  # Get the column itself
+        column_type = column_data.dtype  # Get the datatype of column elements
+        print(f"Column '{column}': {count} NaN values; datatype: {column_type}")
+
+    # Count rows with NaN values
+    rows_with_nan = df.isnull().any(axis=1).sum()
+    print(f"\nNumber of rows with at least one NaN value: {rows_with_nan} of {len(df.index)}")
+    print("\nOrgans: " + ", ".join(list(set(df["Organ"]))))
+    nps = "\n- ".join(list(set(df['NP_Type'])))
+    print(f'\nIncluded NPs: \n- {nps}')
+    shapes = "\n- ".join(list(set(df['NP_Shape'])))
+    print(f'\nShapes: \n- {shapes}')
+    ligands = "\n- ".join(list(set(df['Ligand'])))
+    print(f'\nLigands used for imaging: \n- {ligands}')
+    coating = "\n- ".join(list(set(df['PEG cover'])))
+    print(f'\nCoating (PEG covers): \n- {coating}')
+
+ 
--- notebooks/scripts/get_full_text_epmc.py
+++ notebooks/scripts/get_full_text_epmc.py
@@ -3,7 +3,7 @@ import re
 import numpy as np
 import json
 
-def get_full_text(id):
+def get_full_text(id, check =False):
 
     base_url_text = "https://www.ebi.ac.uk/europepmc/webservices/rest/{}/fullTextXML"
     USER_AGENT = "Mozilla/5.0"
@@ -13,22 +13,26 @@ def get_full_text(id):
     response_code = response.status_code
     try:
         if response_code < 200 or response_code >= 300:
-            #print(f"Failed to retrieve data for {id}. Response code: " +str(response_code))
+            print(f"Failed to retrieve data for {id}. Response code: {str(response_code)} / url: {url}")
             pass
         else:
             response_text = response.text
-            body_content = ""
-            regex = "<body(.*?)</body"
-            pattern = re.compile(regex, re.DOTALL)
-            matcher = pattern.search(response_text)
-            if matcher:
-                body_content = matcher.group(1)
-                cleaned_text = re.sub("<.*?>", "", body_content)  #remove  all XML tags
-                cleaned_text = re.sub("&[#\\w]+?;", "", cleaned_text)  #remove HTML entities
-                cleaned_text = re.sub("\\s+", " ", cleaned_text)  #replace multiple whitespaces with a single space
-                cleaned_text = cleaned_text.replace(">", "") # left fromthe body tag if <body>
-                cleaned_text = cleaned_text.strip()  #  remove leadingtrailing whitespaces
-                return cleaned_text
+
+            cleaned_text = re.sub("<.*?>", "", response_text)  #remove all XML tags
+            cleaned_text = re.sub("&[#\\w]+?;", "", cleaned_text) #remove HTML entities
+            cleaned_text = re.sub("\\s+", " ", cleaned_text) #replace multiple whitespaces with a single space
+            cleaned_text = cleaned_text.replace(">", "") # leftfromthe    body tag if <body>
+            cleaned_text = cleaned_text.strip()  #  removeleadingtrailing whitespaces
+            return cleaned_text
     except Exception as e:
-        #print(f"An error occurred for {id}: " + str(e))
+        print(f"Not available: {id} / " + str(e))
         pass
+
+def check_full_texts(ids):
+    available = []
+    for id in ids:
+        is_ft = get_full_text(id, check = True)
+        if is_ft:
+            print(id)
+            available.append(id)
+    return available
